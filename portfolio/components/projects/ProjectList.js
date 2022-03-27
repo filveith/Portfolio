630bb294dd930c03d4d280ab13738edfcccc291f
Fil Veith <pub.ve@gmx.com>
2022-03-27 11:29:40 +0200

    css home page start

--- portfolio/components/projects/ProjectList.js
+++ portfolio/components/projects/ProjectList.js
@@ -1,14 +1,14 @@
 import Project from "./Project";
 import ProjectShort from "./ProjectShort";
-import classes from './ProjectList.module.css'
+import classes from "./ProjectList.module.css";
 
 export default function ProjectList(props) {
 	return (
 		<div id="ProjectList" className={classes.container}>
 			{props.projects.map((project) => (
-				<ProjectShort
-					project={project}
-				/>
+				<div className={classes.child}>
+					<ProjectShort project={project} />
+				</div>
 			))}
 		</div>
 	);
--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -6,13 +6,48 @@ import ProjectList from "../components/projects/ProjectList";
 import maria from "./api/db_connexion";
 const { pool } = maria;
 
-function HomePage(props) {
+import classes from "../styles/global.module.css";
+import ids from "../styles/index.module.css";
+
+export default function HomePage(props) {
 	return (
-		<Fragment>
-			<h1>Who I am </h1>
-			<h1>My Projects</h1>
-			<ProjectList projects={props.projects} />
-		</Fragment>
+		<div className={classes.home}>
+			<div id="me" className={classes.me}>
+				<h1>Who I am </h1>
+			</div>
+
+			<div id="my_projects">
+				<div className={classes.box}>
+					<h1>My Projects</h1>
+					<ProjectList projects={props.projects} />
+				</div>
+			</div>
+
+			<div id="currently" className={classes.currently}>
+				<h1>Currently doing this and that</h1>
+				<div id={ids.details_currently} className={classes.box}>
+					<div id={ids.school} className={classes.box}>
+						<h2>Studying</h2>
+						<p>Epitech</p>
+					</div>
+					<div id={ids.work} className={classes.box}>
+						<h2>Working</h2>
+						<p>Big company</p>
+					</div>
+					<div id={ids.projects} className={classes.box}>
+						<h2>Projects</h2>
+						<td>
+							<tr>Better Spotify</tr>
+							<tr>Solitaire solver</tr>
+						</td>
+					</div>
+				</div>
+			</div>
+
+			<div id="contact">
+				<h1>Contact Me</h1>
+			</div>
+		</div>
 	);
 }
 
@@ -34,5 +69,3 @@ export async function getStaticProps() {
 		// revalidate: 1 // Usefull when the data changes very often in this case we refresh it every second
 	};
 }
-
-export default HomePage;
