3dbd30742ba9fae4764624f5a7edd0697f2cdae4
Fil <pub.ve@gmx.com>
2022-04-24 13:23:25 +0200

    projects working again

--- portfolio/pages/projects/index.js
+++ portfolio/pages/projects/index.js
@@ -8,69 +8,24 @@ export default function Projects(props) {
 }
 
 export async function getStaticProps() {
-	let projects = await fetch("http://filveith.ddns.net:2000/projects", {
+	const api_data = await fetch("http://localhost:3000/api/projects", {
 		method: "GET",
 		headers: {
 			Accept: "application/json",
 		},
 	});
-	delete projects.meta;
-
-	for (let i = 0; i < projects.length; i++) {
-		let tag_project = await fetch(
-			"http://filveith.ddns.net:2000/projects_tags/" + projects[i].projectId,
-			{
-				method: "GET",
-				headers: {
-					Accept: "application/json",
-				},
-			}
-		);
-
-		delete tag_project.meta;
-
-		let tags;
-
-		for (let j = 0; j < tag_project.length; j++) {
-			try {
-				let res = await fetch(
-					"http://filveith.ddns.net:2000/tags/" + tag_project[j].tagId,
-					{
-						method: "GET",
-						headers: {
-							Accept: "application/json",
-						},
-					}
-				);
-
-				delete res.meta;
-
-				if (tags) {
-					tags = [...tags, res[0]];
-				} else {
-					tags = [res[0]];
-				}
-			} catch (error) {}
-		}
-		if (tags) {
-			projects[i] = {
-				...projects[i],
-				tags: [...tags],
-			};
-		}
-	}
-	// const images = await pool.query("SELECT * FROM Images");
-	// await pool.end()
+	const projects = (await api_data.json()).projects;
 
 	return {
 		props: {
 			projects: projects.map((project) => ({
-				title: project.title,
-				description: project.description,
-				github: project.github,
-				tags: project.tags ?? {},
-				id: project.title,
-				key: project.projectId,
+				title: project.details.title,
+				description: project.details.description,
+				github: project.details.github,
+				images: project.images,
+				id: project.details.projectId,
+				key: project.details.projectId,
+				tags: project.tags,
 			})),
 		},
 		// revalidate: 1 // Usefull when the data changes very often in this case we refresh it every second
